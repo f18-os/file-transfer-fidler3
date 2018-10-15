037de62de5c2339d468c8171bb7888b2ee9cf259
fidler3 <wfidler@miners.utep.edu>
2018-10-14 19:43:23 -0600

    working sort of still needs work

--- file-transfer-lab/client/fileclient.py
+++ file-transfer-lab/client/fileclient.py
@@ -56,38 +56,83 @@ if s is None:
     print('could not open socket')
     sys.exit(1)
 
-
     
 #print("sending hello world")
-check = True
 
-while check:
-    f = input("Enter filename to get from server\n")
-    if os.path.isfile(f):
-        print("This file already exists in directory")
-    else:
-        check = False
-txtfile = f.encode('utf-8')
-framedSend(s, txtfile, debug)
-txtfile = f
-
-with open("copy " + txtfile, 'wb') as r:
-    while True:
-        input = s.recv(100)
-        if input == b'LETSGO':
-            continue #lets begin reading file
-        elif input == b'TDBABY':
-            print("Done writing")
-            break
-        elif input == b'WRONG': #wrong and tdbaby serve the similar purposes, but are different
-            print("File does not exist exiting")
-            break
+def puts(c):
+    
+        s.send(b'PUTS')
+        #f = input("Enter file to put in server")
+        txtfile = c[1].encode('utf-8')
+        framedSend(s, txtfile, debug)
+        message = framedReceive(s, debug)
+        if message == b'begin':
+            getruns = open(c[1], "rb") #open file for reading bytes
+            s.send(b'LETSGO') #Lets drive this file to the end zone.. football references
+            while True:
+                run = getruns.read(100)
+                s.send(run)
+                print("sent ") #sent 100 bytes
+                if not run:
+                    print("Done")
+                    #s.shutdown(socket.SHUT_WR)
+                    #s.send(b'TDBABY')
+                    break
+            s.send(b'TDBABY') #Touchdown you're done
+            getruns.close()
+            s.close()
         else:
-            r.write(input)
-    r.close()
-    print("All done")
-s.close()
+            print("File already exists in server")
+            s.close()
+
+
+def get(f):
+    s.send(b'GETS')
+    check = True
+    while check:
+        #f = input("Enter filename to get from server\n")
+        if os.path.isfile(f):
+            print("This file already exists in directory")
+        else:
+            check = False
+            txtfile = f.encode('utf-8')
+            framedSend(s, txtfile, debug)
+            txtfile = f
+
+            
+    with open("copy " + txtfile, 'wb') as r:
+        while True:
+            input = s.recv(100)
+            if input == b'LETSGO':
+                continue #lets begin reading file
+            elif input == b'TDBABY':
+                print("Done writing")
+                break
+            elif input == b'WRONG': #wrong and tdbaby serve the similar purposes, but are different
+                print("File does not exist exiting")
+                break
+            else:
+                r.write(input)
+        r.close()
+        print("All done")
+        s.close()
+
+while True:
+    command = input("put or get <filename>")
+    c = command.split()
+    print(c)
+    filename = c[1]
+    if c[0] == "put":
+        puts(c)
+    elif c[0] == "get":
+        get(c)
+    else:
+        print("Incorrect command")
 #print("received:", framedReceive(s, debug))
+
+
+
+
 
 #print("sending hello world")
 #framedSend(s, b"hello world", debug)
--- file-transfer-lab/server/fileServer.py
+++ file-transfer-lab/server/fileServer.py
@@ -27,6 +27,60 @@ lsock.bind(bindAddr)
 lsock.listen(5)
 print("listening on:", bindAddr)
 
+def receive():
+    take = framedReceive(sock, debug)
+    print(take)
+    f = take.decode('utf-8')
+    if os.path.isfile(f):
+        print("This file already exists in directory")
+        sock.send(b'wrrong')
+        sys.exit(0)
+    else:
+        framedSend(sock, b'begin', debug)
+        #txtfile = f.encode('utf-8')
+       #framedSend(sock, txtfile, debug)
+        txtfile = f
+    with open("copy " + txtfile, 'wb') as r:
+        while True:
+            a = sock.recv(100)
+            if a == b'LETSGO':
+                continue #lets begin reading file
+            elif a == b'TDBABY':
+                print("Done writing")
+                break
+            elif a == b'WRONG': #wrong and tdbaby serve the similar purposes, but are different
+                print("File does not exist exiting")
+                break
+            else:
+               # print(a.decode('utf-8'))
+                r.write(a)
+        r.close()
+        print("All done")
+        sock.close()
+
+def send():
+     payload = framedReceive(sock, debug)
+     if debug: print("rec'd: ", payload)
+     payload = payload.decode('utf-8')
+     print("sending " + payload)
+     if os.path.isfile(payload):
+         getruns = open(payload, "rb") #open file for reading bytes
+         sock.send(b'LETSGO') #Lets drive this file to the end zone.. football references
+         while True:
+             run = getruns.read(100)
+             sock.send(run)
+             print("sent ") #sent 100 bytes
+             if not run:
+                 print("Done")
+                 break
+             sock.send(b'TDBABY') #Touchdown you're done
+             getruns.close()
+             sys.exit(0)
+     else:
+         print("File does not exist informing client and  exiting program")
+         sock.send(b'WRONG')
+         sys.exit(0)
+    
 while True:
     sock, addr = lsock.accept()
 
@@ -35,26 +89,34 @@ while True:
     if not os.fork():
         print("new child process handling connection from", addr)
         while True:
-            payload = framedReceive(sock, debug)
-            if debug: print("rec'd: ", payload)
-            if not payload:
-                break
-            payload = payload.decode('utf-8')
-            print("sending " + payload)
-            if os.path.isfile(payload):
-                getruns = open(payload, "rb") #open file for reading bytes
-                sock.send(b'LETSGO') #Lets drive this file to the end zone.. football references
-                while True:
-                    run = getruns.read(100)
-                    sock.send(run)
-                    print("sent ") #sent 100 bytes
-                    if not run:
-                        print("Done")
-                        break
-                sock.send(b'TDBABY') #Touchdown you're done
-                getruns.close()
-                sys.exit(0)
+            command = sock.recv(4) #4 bytes for PUTS or GETS
+            print(command)
+            if command == b'PUTS':
+                receive()
+                sock.close()
+            elif command == b'GETS':
+                send()
+           # payload = framedReceive(sock, debug)
+           # if debug: print("rec'd: ", payload)
+           # if not payload:
+           #     break
+           # payload = payload.decode('utf-8')
+           # print("sending " + payload)
+           # if os.path.isfile(payload):
+           #     getruns = open(payload, "rb") #open file for reading bytes
+           #     sock.send(b'LETSGO') #Lets drive this file to the end zone.. football references
+           #     while True:
+           #         run = getruns.read(100)
+           #         sock.send(run)
+           #         print("sent ") #sent 100 bytes
+           #         if not run:
+           #             print("Done")
+           #             break
+           #     sock.send(b'TDBABY') #Touchdown you're done
+           #     getruns.close()
+           #     sys.exit(0)
             else:
-                print("File does not exist informing client and  exiting program")
-                sock.send(b'WRONG')
+                print("Not a good command")
                 sys.exit(0)
+                break;
+            sys.exit(0)
